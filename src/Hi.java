5eea994b02fba06e74ff81e7e258a67f16c7b5be
thidatip104 <thidatip199045@gmail.com>
2022-06-10 11:38:43 +0700

    first commit

---
+++ src/Hi.java
@@ -0,0 +1,13 @@
+public class Hi {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int sum;
+        a = 5;
+        b = 10;
+        sum = a+b ;
+        System.out.println("Hello world!!!");
+        System.out.println(100+5);
+        System.out.println(sum);
+    }
+}
